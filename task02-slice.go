17b190e852f7b85fe447715eca9d898508bd6bf2
vital <vitalikgk@mail.ru>
2022-05-06 00:14:40 +0400

    test

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,11 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	//Place your code here
-	return
+	for i := int64(len(input)/2 - 1); i >= 0; i-- {
+		opp := int64(len(input)) - 1 - i
+		input[i], input[opp] = input[opp], input[i]
+
+		//	fmt.Printd("%d", input[])
+	}
+	return input
 }
--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,29 @@
 package homework
 
+import (
+	"fmt"
+	"sort"
+)
+
 func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
-	return
+
+	type kv struct {
+		k int
+		v string
+	}
+	kvs := make([]kv, 0, len(input))
+	for k, v := range input {
+		kvs = append(kvs, kv{k, v})
+	}
+	sort.Slice(kvs, func(i, j int) bool {
+		return kvs[i].k < kvs[j].k
+	})
+	n := make([]string, 0, len(input))
+	for k, v := range kvs {
+		fmt.Println(kvs[k].v, v)
+		nn := kvs[k].v
+		n = append(n, string(nn))
+	}
+
+	return n
 }
